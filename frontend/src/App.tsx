25cf57040cd0eb7f1409db65d315a1c14baf62e6
kirilchobansky <kirilchobansky@gmail.com>
2026-07-26 12:25:28 +0300

    Add analytics to the site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import './App.css';
-import MatrixRain from './layout/matrix-rain/MatrixRain';
-import Navbar from './layout/navbar/Navbar';
-import Home from './pages/home/Home';
-import { useTheme } from './context/ThemeContext'; 
+import "./App.css";
+import MatrixRain from "./layout/matrix-rain/MatrixRain";
+import Navbar from "./layout/navbar/Navbar";
+import Home from "./pages/home/Home";
+import { useTheme } from "./context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const { theme } = useTheme();
   return (
-    <div className='main-root'>
-      
-      {/* Only render Matrix Rain if theme is 'cyber' */}
-      {theme === 'cyber' && <MatrixRain />}
-      
+    <div className="main-root">
+      {theme === "cyber" && <MatrixRain />}
+
       <Navbar />
       <Home />
+      <Analytics />
     </div>
   );
 }
